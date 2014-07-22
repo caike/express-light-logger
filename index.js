d077a10b113662c7b20f0b903eefc78e3f1a9dfb
Carlos Souza <carloshrsouza@gmail.com>
2014-07-22 19:28:07 -0400

    Initial code.

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function() {
+
+  return function(req, res, next) {
+    var start = process.hrtime();
+    var stream = process.stdout;
+
+    stream.write('\nRequest started\n');
+
+    res.on('finish', function() {
+      var diff = process.hrtime(start);
+      var duration = diff[0] * 1e3 + diff[1] * 1e-6
+
+      stream.write('\nResponse sent in ' + duration.toFixed(3) + ' milliseconds \n');
+    });
+
+    next();
+  }
+}
+
